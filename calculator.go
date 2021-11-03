4b5524f93312831c6233652e770e489fa34f9726
obr-note <k33mdukb9n@yahoo.co.jp>
2021-11-03 16:50:12 +0900

    add index template

--- calculator.go
+++ calculator.go
@@ -1,7 +1,36 @@
 package calculator
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
 
 func Handler() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/result", result)
+	http.ListenAndServe(":8080", nil)
 	fmt.Println("こんにちは!")
+}
+
+func index(w http.ResponseWriter, rq *http.Request) {
+	// fmt.Fprintln(w, "こんにちは")
+	type Inventory struct {
+		Formula string
+		Count   uint
+	}
+	sweaters := Inventory{"wool", 17}
+	fmt.Println(rq.FormValue("formula"))
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		panic(err)
+	}
+	err = tmpl.Execute(w, sweaters)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func result(w http.ResponseWriter, rq *http.Request) {
+
 }
